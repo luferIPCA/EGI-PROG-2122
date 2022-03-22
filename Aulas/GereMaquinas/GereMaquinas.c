e8b880487cc2fa29692ef911b2767500f05544d2
lufer <lufer@ipca.pt>
2022-03-22 22:03:37 +0000

    Listas Ligadas

---
+++ Aulas/GereMaquinas/GereMaquinas.c
@@ -0,0 +1,33 @@
+/**
+*  @file GereMaquinas.c
+ * @author lufer
+ * @email lufer@ipca.pt
+ * @date 2022
+ * @brief Lista Ligadas Simples
+ *
+ *	Manipulação de Lista Ligada Simples de Máquinas
+
+ * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ * @see http://www.stack.nl/~dimitri/doxygen/commands.html
+ * @see http://fnch.users.sourceforge.net/doxygen_c.html
+ *
+ * @bug bugs desconhecidos.
+*/
+
+#include "Dados.h"
+#include "Maquinas.h"
+
+
+
+int main() {
+
+	Maquina* inicoLista;	
+	inicoLista= NULL;
+
+	Maquina* x = CriaMaquina(12, "Corte");
+	inicoLista = InsereMaquina(inicoLista, x);
+
+	x = CriaMaquina(13, "Pintar");
+	inicoLista = InsereMaquina(inicoLista, x);
+
+}
